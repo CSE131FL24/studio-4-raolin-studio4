0a261975266941e10a998ff3ce1c70f1956255d6
Lenovo <Lenovo@meow.dhcp.wustl.edu>
2024-10-02 14:11:25 -0500

    studio4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,32 @@
 package studio4;
 
 import java.awt.Color;
+import java.awt.Font;
+
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+
+		StdDraw.setPenColor(65, 50, 125);
+		StdDraw.filledRectangle(0.5, 0.5, 0.45, 0.25);
+
+		for (double i = 0.40; i < 0.60; i=i+0.09) {
+			StdDraw.setPenColor(Color.white);
+			StdDraw.filledRectangle(0.5, i, 0.45, 0.01);
+
+		}
+
+		StdDraw.setPenColor(243, 145, 31);
+		StdDraw.filledRectangle(0, 0.5, 0.1, 0.25);
+
+		Font font = new Font("Arial", Font.BOLD, 40);
+		StdDraw.setFont(font);
+		StdDraw.text(0.5, 0.65, "eclipsedom");
+		Font font2 = new Font("Arial", Font.BOLD, 30);
+		StdDraw.setFont(font2);
+		StdDraw.text(0.5, 0.315, "//code. debug. push.//");
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,37 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+
+		String shapeType = in.next();
+		System.out.println(shapeType);
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		System.out.println(redComponent + " " + greenComponent + " " + blueComponent);
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+
+		if (shapeType.equals("rectangle")) {
+			if (isFilled == true) {
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else {
+				StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		} 
+
+		else if (shapeType.equals("ellipse")) {
+			if (isFilled == true) {
+				StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else {
+				StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			} 
+		} 
 	}
 }
